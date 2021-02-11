b2f871bcb819b5f70b06da40f9cee4cb7b982c0e
Arief <riff.sense@gmail.com>
2021-02-12 01:03:23 +0700

    config, home, foods template

---
+++ src/views/Foods.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="foods">
+    <Navbar />
+    <h1>Foods</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Foods',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
