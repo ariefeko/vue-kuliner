fc80852489b75096e6478e3399b0d55df0a55ab9
Arief <riff.sense@gmail.com>
2021-02-12 22:41:29 +0700

    food detail

--- src/views/FoodDetail.vue
+++ src/views/FoodDetail.vue
@@ -2,7 +2,6 @@
   <div class="food-detail">
     <Navbar />
     <div class="container">
-
       <!-- Breadcrumb -->
       <div class="row mt-5">
         <div class="col">
@@ -14,7 +13,9 @@
               <li class="breadcrumb-item">
                 <router-link to="/foods" class="text-dark">Foods</router-link>
               </li>
-              <li class="breadcrumb-item active" aria-current="page">Food Order</li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Food Order
+              </li>
             </ol>
           </nav>
         </div>
@@ -23,23 +24,37 @@
       <!-- Product Detail -->
       <div class="row">
         <div class="col-md-6">
-          <img :src="productWithImage.pic" class="img-fluid shadow">
+          <img :src="productWithImage.pic" class="img-fluid shadow" />
         </div>
         <div class="col-md-6">
-          <h2><strong>{{ product.nama }}</strong></h2>
-          <hr>
-          <h4>Harga : <strong>Rp. {{ product.harga }}</strong></h4>
-          <form class="mt-4">
+          <h2>
+            <strong>{{ product.nama }}</strong>
+          </h2>
+          <hr />
+          <h4>
+            Harga : <strong>Rp. {{ product.harga }}</strong>
+          </h4>
+          <form class="mt-4" v-on:submit.prevent>
             <div class="form-group">
               <label for="jumlah_pemesanan">Jumlah Pesan</label>
-              <input type="number" class="form-control">
+              <input
+                type="number"
+                class="form-control"
+                v-model="pesan.jumlah_pemesanan"
+              />
             </div>
             <div class="form-group">
               <label for="keterangan">Keterangan</label>
-              <textarea class="form-control" placeholder="Keterangan spt: Pedes, Nasi Setengah ..."></textarea>
+              <textarea
+                v-model="pesan.keterangan"
+                class="form-control"
+                placeholder="Keterangan spt: Pedes, Nasi Setengah ..."
+              ></textarea>
             </div>
 
-            <button type="submit" class="btn btn-success"><b-icon-cart></b-icon-cart> Order</button>
+            <button type="submit" class="btn btn-success" @click="pemesanan">
+              <b-icon-cart></b-icon-cart> Order
+            </button>
           </form>
         </div>
       </div>
@@ -48,8 +63,8 @@
 </template>
 
 <script>
-import Navbar from "@/components/Navbar.vue"
-import axios from "axios"
+import Navbar from "@/components/Navbar.vue";
+import axios from "axios";
 
 export default {
   name: "FoodDetail",
@@ -58,28 +73,60 @@ export default {
   },
   data() {
     return {
-      product: [],
-    }
+      product: {},
+      pesan: {},
+    };
   },
   computed: {
     productWithImage() {
       return {
         ...this.product,
-        pic: this.product.gambar && require(`../assets/images/${this.product.gambar}`)
-      }
-    }
+        pic:
+          this.product.gambar &&
+          require(`../assets/images/${this.product.gambar}`),
+      };
+    },
   },
   // get data from backend and put it to products array above
   methods: {
     setProduct(data) {
-      this.product = data
-    }
+      this.product = data;
+    },
+    pemesanan() {
+      if (
+        this.pesan.jumlah_pemesanan &&
+        Number(this.pesan.jumlah_pemesanan) >= 1
+      ) {
+        this.pesan.products = this.product;
+        axios
+          .post("http://localhost:3000/keranjangs", this.pesan)
+          .then(() =>
+            this.$toast.success("Sukses Masuk Keranjang", {
+              type: "success",
+              position: "top-right",
+              duration: 3000,
+              dismissable: true,
+            })
+          )
+          .catch((err) => console.log(err));
+      } else {
+        this.$toast.error(
+          "Jumlah Pemesanan Harus Diisi dan Minimal 1 Pemesanan",
+          {
+            type: "error",
+            position: "top-right",
+            duration: 3000,
+            dismissable: true,
+          }
+        );
+      }
+    },
   },
   mounted() {
     axios
-      .get("http://localhost:3000/products/"+this.$route.params.id)
+      .get("http://localhost:3000/products/" + this.$route.params.id)
       .then((response) => this.setProduct(response.data))
-      .catch((error) => console.log(error))
-  }
-}
+      .catch((error) => console.log(error));
+  },
+};
 </script>
